b08119af527538d8628c6170621456714bd3c884
dimethma <ranaweeradimethma@gmail.com>
2025-04-08 12:20:57 +0530

    data management created

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,12 @@ Route::get('/air-quality', function () {
     return view('pages.user.airquality');
 })->name('airquality');
 
+Route::get('/admin/data-management', function () {
+    return view('pages.admin.data-management');
+});
+
+
 use App\Http\Controllers\User\AirQualityController;
 
 Route::get('/historical-data', [AirQualityController::class, 'historicalData'])->name('historical.data');
-
 
