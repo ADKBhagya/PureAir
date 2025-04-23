4504c966085abc43e0cedeaf22a044e30fb25f06
Kaushani Bhagya <140969195+ADKBhagya@users.noreply.github.com>
2025-04-23 13:33:45 +0530

    chore(routes): update routes for login and dashboard redirection

--- routes/web.php
+++ routes/web.php
@@ -60,7 +60,9 @@ Route::post('/admin/webmaster-login', [WebMasterAuthController::class, 'login'])
 | Dummy Page:  Data Management
 |--------------------------------------------------------------------------
 */
-
+Route::get('/admin/data-management', function () {
+    return view('pages.admin.data-management');
+})->name('admin.data.management');
 
 
 
@@ -69,7 +71,9 @@ Route::post('/admin/webmaster-login', [WebMasterAuthController::class, 'login'])
 | Dummy Page: Dashboard
 |--------------------------------------------------------------------------
 */
-
+Route::get('/admin/dashboard', function () {
+    return view('pages.admin.dashboard');
+})->name('admin.dashboard');
 
 /*
 |--------------------------------------------------------------------------
@@ -85,14 +89,18 @@ Route::get('/admin/aqi-status', function () {
 | Dummy Page: Admin user management
 |--------------------------------------------------------------------------
 */
-
+Route::get('/admin/user-management', function () {
+    return view('pages.admin.user-management');
+})->name('admin.user.management');
 
 /*
 |--------------------------------------------------------------------------
 | Dummy Page: Sensor management
 |--------------------------------------------------------------------------
 */
-
+Route::get('/admin/sensor-management', function () {
+    return view('pages.admin.sensor-management');
+})->name('admin.sensor.management');
 
 
 /*
@@ -100,3 +108,8 @@ Route::get('/admin/aqi-status', function () {
 | Dummy Page: Alert Configuration
 |--------------------------------------------------------------------------
 */
+Route::get('/admin/alert-configuration', function () {
+    return view('pages.admin.alerts'); // 👈 match the actual blade file path
+})->name('admin.alert');
+
+
